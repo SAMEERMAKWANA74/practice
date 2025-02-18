fd476badef3143487208b7f87842f17115e92587
SAMEERMAKWANA74 <SAMEERMAKWANA74@gmial.com>
2025-02-18 16:40:11 +0530

    this is push by ij

--- src/javatask2demo.java
+++ src/javatask2demo.java
@@ -1,15 +1,49 @@
-
-import java.sql.SQLOutput;
 import java.util.Scanner;
-
 public class javatask2demo {
-    public static void main(String arg[])
-    {
+    public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
-        System.out.println("Enter number of Subjects:-");
-        int numofsubjects = scanner.nextInt();
+        System.out.println("Enter a Numbers of Subjects");
+        int numofSybjects = scanner.nextInt();
 
+        int totalmarks=0;
+        for(int i=0;i<numofSybjects;i++)
+        {
+            System.out.println("Enter Makrs of :"+i+" Subject");
+            int marks=scanner.nextInt();
+            totalmarks += marks;
+        }
+
+
+
+        double pertageofmarks = (double) totalmarks/numofSybjects;
+        System.out.println("---- Result ----");
+        System.out.println("Total Marks:-"+totalmarks);
+        System.out.println("Percentage:-"+pertageofmarks);
+
+        if(pertageofmarks>90)
+        {
+            System.out.println("Garde is :- +A");
+        }
+        else if(pertageofmarks>80)
+        {
+            System.out.println("A");
+        }
+        else if(pertageofmarks>=70)
+        {
+            System.out.println("B");
+        }
+        else if(pertageofmarks>60)
+        {
+            System.out.println("C");
+        }
+        else if(pertageofmarks>50)
+        {
+            System.out.println("D");
+        }
+        else{
+            System.out.println("F");
+        }
 
     }
 }
