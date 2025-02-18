57c656037b0097ac352de19dad7779db2e1b0ce2
SAMEERMAKWANA74 <SAMEERMAKWANA74@gmial.com>
2025-02-18 16:37:57 +0530

    this is push by ij

---
+++ src/javatask2demo.java
@@ -0,0 +1,15 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class javatask2demo {
+    public static void main(String arg[])
+    {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter number of Subjects:-");
+        int numofsubjects = scanner.nextInt();
+
+
+    }
+}
